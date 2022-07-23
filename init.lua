026fdb3b4af21e4d09289d4071e2f9a34f72744b
DevconDev <devon.hopkins333@gmail.com>
2022-07-23 16:29:12 -0300

    Added happy_shark targets

--- init.lua
+++ init.lua
@@ -70,7 +70,71 @@ Config.CircleZones = {
 }
 
 Config.BoxZones = {
-
+	["happyshark"] = { -- This can be a string or a number
+    name = "happyshark", -- This is the name of the zone recognized by PolyZone, this has to be unique so it doesn't mess up with other zones
+    coords = vector3(376.67, -972.22, 29.44), -- These are the coords for the zone, this has to be a vector3 and the coords have to be a float value, fill in x, y and z with the coords
+    length = 1.5, -- The length of the boxzone calculated from the center of the zone, this has to be a float value
+    width = 0.4, -- The width of the boxzone calculated from the center of the zone, this has to be a float value
+    heading = 90.0, -- The heading of the boxzone, this has to be a float value
+    debugPoly = false, -- This is for enabling/disabling the drawing of the box, it accepts only a boolean value (true or false), when true it will draw the polyzone in green
+    minZ = 25.84, -- This is the bottom of the boxzone, this can be different from the Z value in the coords, this has to be a float value
+    maxZ = 29.84, -- This is the top of the boxzone, this can be different from the Z value in the coords, this has to be a float value
+    options = { -- This is your options table, in this table all the options will be specified for the target to accept
+      { -- This is the first table with options, you can make as many options inside the options table as you want
+        num = 1, -- This is the position number of your option in the list of options in the qb-target context menu (OPTIONAL)
+        type = "server", -- This specifies the type of event the target has to trigger on click, this can be "client", "server", "command" or "qbcommand", this is OPTIONAL and will only work if the event is also specified
+        event = "QBCore:ToggleDuty", -- This is the event it will trigger on click, this can be a client event, server event, command or qbcore registered command, NOTICE: Normal command can't have arguments passed through, QBCore registered ones can have arguments passed through
+        icon = 'fas fa-circle', -- This is the icon that will display next to this trigger option
+        label = 'Toggle Duty', -- This is the label of this option which you would be able to click on to trigger everything, this has to be a string
+        job = 'happyshark', -- This is the job, this option won't show up if the player doesn't have this job, this can also be done with multiple jobs and grades, if you want multiple jobs you always need a grade with it: job = {["police"] = 0, ["ambulance"] = 2},
+        drawDistance = 10.0, -- This is the distance for the sprite to draw if Config.DrawSprite is enabled, this is in GTA Units (OPTIONAL)
+        drawColor = {255, 255, 255, 255}, -- This is the color of the sprite if Config.DrawSprite is enabled, this will change the color for this PolyZone only, if this is not present, it will fallback to Config.DrawColor, for more information, check the comment above Config.DrawColor (OPTIONAL)
+        successDrawColor = {30, 144, 255, 255}, -- This is the color of the sprite if Config.DrawSprite is enabled, this will change the color for this PolyZone only, if this is not present, it will fallback to Config.DrawColor, for more information, check the comment above Config.DrawColor (OPTIONAL)
+      }
+    },
+    distance = 2.5, -- This is the distance for you to be at for the target to turn blue, this is in GTA units and has to be a float value
+  },
+  ["happysharkbank"] = {
+	name = "HappySharkBank",
+	coords = vector3(375.11, -978.53, 29.44),
+	length = 0.5,
+	width = 0.1,
+	heading = 10.0,
+	debugPoly = false,
+	minZ = 26.04,
+	maxZ = 30.04,
+	options = {
+		{
+			type = "client",
+			event = "qb-banking:openBankScreen",
+			icon = "fas fa-sign-in-alt",
+			label = "Sign In",
+			job = "happyshark",
+		},
+	},
+	distance = 2.5
+},
+["mechaniccraft"] = {
+        name = "Mechanic Toolbox",
+        coords = vector3(136.7, -3051.41, 7.04),
+        length = 1,
+        width = 1,
+        heading = 0,
+        debugPoly = false,
+        minZ=3.44,
+        maxZ=7.44,
+        options = {
+            {
+                type = "client",
+                event = "craft:mechanicparts", 
+                icon = "fas fa-wrench",
+                label = "Craft",
+                job = "mechanic",
+            },
+        },
+        distance = 2.5
+    },
+   
 }
 
 Config.PolyZones = {
@@ -78,7 +142,24 @@ Config.PolyZones = {
 }
 
 Config.TargetBones = {
-
+["mechanic"] = {
+        bones = {
+            "door_dside_f",
+            "door_dside_r",
+            "door_pside_f",
+            "door_pside_r"
+        },
+        options = {
+            {
+                type = "client",
+                event = "craft:vehmenu",
+                icon = "fad fa-key",
+                label = "Vehicle Menu",
+                job = "mechanic",
+            },
+        },
+        distance = 3.0
+    },
 }
 
 Config.TargetModels = {
